94e9b459c27bef29c1e3ea9d60417005626d4c27
Ahmad A. Duhoki <ahmad.a.duhoki99@gmail.com>
2020-06-22 09:54:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 class Formatter {
-  static capitalize(string){
+  static capitalize(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
   
   static sanitize(string) {
     return string.replace(/[^A-Za-z0-9-' ]+/g, '');
+  }
+  
+  static titleize(string) {
+    
   }
 }
